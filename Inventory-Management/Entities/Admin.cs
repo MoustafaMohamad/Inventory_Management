6cf439f78f0001d4f4a551d3ee87c142948ed293
MoustafaMohamad <moustafamohamad.2020@azhar.edu.eg>
2024-10-28 16:08:13 +0300

    adding the Admin and User Models with the relation between them

---
+++ Inventory-Management/Entities/Admin.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory_Management.Entities
+{
+    public class Admin
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int UserId { get; set; } 
+        public User User { get; set; }
+    }
+}
