0512747fa98db753ac2be8adcab7eef6bf5ada60
Mohamed Ahmed Salah Ibrahim <44170867+mohmedahmedsalah@users.noreply.github.com>
2024-11-09 15:08:41 +0200

    get all transaction

---
+++ Inventory-Management/Features/InventoryTransactions/GetAllTransaction/Queries/GetAll_InventoryTransactionQuery.cs
@@ -0,0 +1,31 @@
+﻿using Common.Helpers;
+using Inventory_Management.Common;
+using Inventory_Management.Common.Exceptions;
+using Inventory_Management.Common.Helpers;
+using Inventory_Management.Entities;
+using Inventory_Management.Features.Products.GetProductDetails.Dtos;
+using MediatR;
+
+namespace Inventory_Management.Features.InventoryTransactions.GetAllTransaction.Queries
+{
+
+    public record GetAll_InventoryTransactionQuery() : IRequest<ResultDto<InventoryTransaction>>;
+
+
+    public class GetAll_InventoryTransactionQueryHandler : BaseRequestHandler<InventoryTransaction, GetAll_InventoryTransactionQuery, ResultDto<InventoryTransaction>> 
+    {
+        
+        public GetAll_InventoryTransactionQueryHandler(RequestParameters<InventoryTransaction> requestParameters) : base(requestParameters)
+        {
+        }
+
+        public async override Task<ResultDto<InventoryTransaction>> Handle(GetAll_InventoryTransactionQuery request, CancellationToken cancellationToken)
+        {
+            var productsQuery = await _repository.GetAll();
+         
+
+            return ResultDto<InventoryTransaction>.Sucess(productsQuery);
+
+        }
+    }
+}
